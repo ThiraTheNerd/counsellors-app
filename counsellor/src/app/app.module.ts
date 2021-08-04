874a31b41fc303b46cd3e3dda84cf6928ed96a0e
Ammoh-Moringa <amos.kiprotich@student.moringaschool.com>
2021-08-04 22:59:34 +0300

    Add counsellor dashboard

--- counsellor/src/app/app.module.ts
+++ counsellor/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ClientsComponent } from './clients/clients.component';
@@ -8,19 +10,34 @@ import { SignInComponent } from './clients/sign-in/sign-in.component';
 import { SignUpComponent } from './clients/sign-up/sign-up.component';
 import { CounsellorComponent } from './counsellor/counsellor.component';
 
+import { FormsModule } from '@angular/forms';
+import { UserService } from './shared/user.service';
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
+import { appRoutes } from './routes';
+import { ToastrModule } from 'ngx-toastr';
+import { SidebarComponent } from './sidebar/sidebar.component';
+
+
 @NgModule({
   declarations: [
     AppComponent,
     ClientsComponent,
     SignInComponent,
     SignUpComponent,
-    CounsellorComponent
+    CounsellorComponent,
+    HomeComponent,
+    SidebarComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    HttpClientModule,
+    ToastrModule.forRoot(),
+    BrowserAnimationsModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ counsellor/src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import * as $ from 'jquery';
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class  SidebarComponent  {
+  title = 'angularbootstrap';
+   ngOnInit() {
+                //Toggle Click Function
+    $("#menu-toggle").click(function(e) {
+      e.preventDefault();
+      $("#wrapper").toggleClass("toggled");
+    });
+  }
+  
+}
