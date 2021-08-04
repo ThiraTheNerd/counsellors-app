7ae7b04a43ae83c1e744ae1dc165852f4499f01c
kiplaa1500 <dennis.kiplangat@student.moringaschool.com>
2021-08-04 13:12:40 +0300

    Sign-up and sign-up page for counsellors

--- counsellor/src/app/counsellor/sign-up/sign-up.component.ts
+++ counsellor/src/app/counsellor/sign-up/sign-up.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { UserService } from 'src/app/shared/user.service';
+import { User } from '../../shared/user.model';
 
 @Component({
   selector: 'app-sign-up',
@@ -6,10 +10,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent implements OnInit {
+  user!: User;
+  emailPattern = "^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$";
 
-  constructor() { }
+  constructor(private userService: UserService, private toastr: ToastrService) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.resetForm();
+  }
+
+  resetForm(form?: NgForm) {
+    if (form != null)
+      form.reset();
+    this.user = {
+      UserName: '',
+      Password: '',
+      Email: '',
+      FirstName: '',
+      LastName: ''
+    }
+  }
+
+
+  OnSubmit(form: NgForm) {
+    this.userService.registerUser(form.value)
+      .subscribe((data: any) => {
+        if (data.Succeeded == true) {
+          this.resetForm(form);
+          this.toastr.success('User registration successful');
+        }
+        else
+          this.toastr.error(data.Errors[0]);
+      });
   }
 
 }
